89f7bd19f4bc376d00cd1c276981774360765385
skl46 <skl4623@outlook.com>
2026-04-30 22:56:33 +0800

    修改tile.h注释的位置（没改代码，不重要）

--- TowerDefenceGame/tile.h
+++ TowerDefenceGame/tile.h
@@ -15,14 +15,11 @@ struct Tile
 		Left,
 		Right
 	};
-	int terrain = 0;
-	//从0开始，地形像素图的索引
-	int decoration = -1;
-	//-1:无装饰，0及以后：装饰像素图的索引
-	int special_flag = -1;
-	//-1: 无特殊，0：防守目标点，1-10：敌人出生点
-	bool has_tower = false;
-	Direction direction = Direction::None;
+	int terrain = 0;//从0开始，地形像素图的索引
+	int decoration = -1;//-1:无装饰，0及以后：装饰像素图的索引
+	int special_flag = -1;//-1: 无特殊，0：防守目标点，1-10：敌人出生点
+	bool has_tower = false;//是否有防御塔
+	Direction direction = Direction::None;//到达这一个单元格后，下一步走向的方向
 };
 
 typedef std::vector<std::vector<Tile>> TileMap;
