288139f5f0c511885dd94f84d89a06e5fd8610f7
skl46 <skl4623@outlook.com>
2026-05-10 23:17:56 +0800

    修正：三种enemy子类的texture图片的行数从6改为5

--- TowerDefenceGame/skeleton_enemy.h
+++ TowerDefenceGame/skeleton_enemy.h
@@ -23,22 +23,22 @@ public:
 
 
 		anim_up.set_loop(true); anim_up.set_interval(0.1);//触发定时器的间隔为100ms
-		anim_up.set_frame_data(tex_skeleton, 6, 4, idx_list_up);
+		anim_up.set_frame_data(tex_skeleton, 5, 4, idx_list_up);
 		anim_down.set_loop(true); anim_down.set_interval(0.1);
-		anim_down.set_frame_data(tex_skeleton, 6, 4, idx_list_down);
+		anim_down.set_frame_data(tex_skeleton, 5, 4, idx_list_down);
 		anim_left.set_loop(true); anim_left.set_interval(0.1);
-		anim_left.set_frame_data(tex_skeleton, 6, 4, idx_list_left);
+		anim_left.set_frame_data(tex_skeleton, 5, 4, idx_list_left);
 		anim_right.set_loop(true); anim_right.set_interval(0.1);
-		anim_right.set_frame_data(tex_skeleton, 6, 4, idx_list_right);
+		anim_right.set_frame_data(tex_skeleton, 5, 4, idx_list_right);
 
 		anim_up_sketch.set_loop(true); anim_up_sketch.set_interval(0.1);
-		anim_up_sketch.set_frame_data(tex_skeleton_sketch, 6, 4, idx_list_up);
+		anim_up_sketch.set_frame_data(tex_skeleton_sketch, 5, 4, idx_list_up);
 		anim_down_sketch.set_loop(true); anim_down_sketch.set_interval(0.1);
-		anim_down_sketch.set_frame_data(tex_skeleton_sketch, 6, 4, idx_list_down);
+		anim_down_sketch.set_frame_data(tex_skeleton_sketch, 5, 4, idx_list_down);
 		anim_left_sketch.set_loop(true); anim_left_sketch.set_interval(0.1);
-		anim_left_sketch.set_frame_data(tex_skeleton_sketch, 6, 4, idx_list_left);
+		anim_left_sketch.set_frame_data(tex_skeleton_sketch, 5, 4, idx_list_left);
 		anim_right_sketch.set_loop(true); anim_right_sketch.set_interval(0.1);
-		anim_right_sketch.set_frame_data(tex_skeleton_sketch, 6, 4, idx_list_right);
+		anim_right_sketch.set_frame_data(tex_skeleton_sketch, 5, 4, idx_list_right);
 
 		max_hp = skeleton_template.hp;//模板里的生命值为最大生命值
 		max_speed = skeleton_template.speed;
