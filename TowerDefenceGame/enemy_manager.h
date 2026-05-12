c35caa1be390025ebe59545e5591556d675117dc
skl46 <skl4623@outlook.com>
2026-05-12 11:01:50 +0800

    新增：1.敌人管理类：负责管理：判断敌人与其他物件（玩家子弹，保护目标点）的碰撞，渲染敌人图片；2.home管理类，在敌人碰撞时执行扣血函数，提供获取当前生命值的方法；3.音频播放类，封装了(使用SDL3的)单次播放, 循环播放音频

---
+++ TowerDefenceGame/enemy_manager.h
@@ -0,0 +1,96 @@
+#ifndef _ENEMY_MANAGER_H_
+#define _ENEMY_MANAGER_H_
+
+#include "enemy.h"
+#include "manager.h"
+#include "config_manager.h"
+#include "home_manager.h"
+
+#include <vector>
+
+class EnemyManager : public Manager<EnemyManager>
+{
+	friend class Manager<EnemyManager>;
+
+public:
+	typedef std::vector<Enemy*> EnemyList;
+
+public:
+	void on_update(double delta)
+	{
+		for (Enemy* enemy : enemy_list)
+			enemy->on_update(delta);
+
+		process_home_collision();
+		process_bullet_collsion();
+
+		remove_invalid_enemy();
+	}
+
+	void on_render(SDL_Renderer* renderer)
+	{
+		for (Enemy* enemy : enemy_list)
+			enemy->on_render(renderer);
+	}
+
+protected:
+	EnemyManager()
+	{
+
+	}
+	~EnemyManager()
+	{
+
+	}
+
+private:
+	EnemyList enemy_list;
+
+private:
+	void process_home_collision()
+	{
+		static const SDL_Point& idx_home = ConfigManager::instance()->map.get_idx_home();
+		static const SDL_FRect& rect_tile_map = ConfigManager::instance()->rect_tile_map;
+
+		static const Vector2 position_home_tile =
+		{
+			rect_tile_map.x + idx_home.x * SIZE_TILE,
+			rect_tile_map.y + idx_home.y * SIZE_TILE,
+		};
+
+		for (Enemy* enemy : enemy_list)
+		{
+			if (enemy->can_remove()) continue;
+
+			const Vector2& position = enemy->get_position();
+
+			if(position.x >= position_home_tile.x
+				&&position.y >= position_home_tile.y
+				&&position.x <= position_home_tile.x + SIZE_TILE
+				&&position.y <= position_home_tile.y + SIZE_TILE)
+			{
+				enemy->make_invalid();
+
+				HomeManager::instance()->decrease_hp(enemy->get_damage());
+			}
+		}
+	}
+
+	void process_bullet_collsion()
+	{
+
+	}
+
+	void remove_invalid_enemy()
+	{
+		enemy_list.erase(std::remove_if(enemy_list.begin(), enemy_list.end(),
+			[](const Enemy* enemy)
+			{
+				bool deleteable = enemy->can_remove();
+				if (deleteable) delete enemy;
+				return deleteable;
+			}), enemy_list.end());
+	}
+};
+
+#endif // !_ENEMY_MANAGER_H_
---
+++ TowerDefenceGame/home_manager.h
@@ -0,0 +1,46 @@
+#ifndef _HOMEMANAGER_H_
+#define _HOMEMANAGER_H_
+
+#include "manager.h"
+#include "config_manager.h"
+#include "resources_manager.h"
+#include "audio_manager.h"
+
+
+class HomeManager : public Manager<HomeManager>
+{
+	friend class Manager<HomeManager>;
+
+public:
+	double get_current_hp_num()
+	{
+		return num_hp;
+	}
+
+	void decrease_hp(double val)
+	{
+		num_hp -= val;
+
+		if (num_hp < 0)
+			num_hp = 0;
+
+		static const ResourcesManager::AudioPool& audio_pool
+			= ResourcesManager::instance()->get_audio_pool();
+
+		//MIX_PlayAudio(nullptr, audio_pool.find(ResID::Sound_HomeHurt)->second);
+
+		AudioManager::instance()->PlayMusic(audio_pool.find(ResID::Sound_HomeHurt)->second);
+	}
+
+protected:
+	HomeManager()
+	{
+		num_hp = ConfigManager::instance()->num_initial_hp;
+	}
+	~HomeManager() = default;
+
+private:
+	double num_hp = 0;
+};
+
+#endif // !_HOMEMANAGER_H_
