4579c380d68f6b16dfafae66bd08530cc9fd22ae
SkllL-de <skl4623@outlook.com>
2026-05-19 00:11:51 +0800

    新增：金币属性类，金币管理类

--- TowerDefenceGame/coin_manager.h
+++ TowerDefenceGame/coin_manager.h
@@ -1,11 +1,19 @@
 #ifndef _COIN_MANAGER_H_
 #define _COIN_MANAGER_H_
 
+#include "coin_prop.h"
 #include "manager.h"
+#include "config_manager.h"
+
+#include <vector>
+
 
 class CoinManager : public Manager<CoinManager>
 {
 	friend class Manager<CoinManager>;
+
+public:
+	typedef std::vector<CoinProp*> CoinPropList;
 
 public:
 	void increase_coin(double val)
@@ -21,18 +29,60 @@ public:
 			num_coin = 0;
 	}
 
+	void on_update(double delta)
+	{
+		for(CoinProp* coin_prop : coin_prop_list)
+			coin_prop->on_update(delta);
+
+		coin_prop_list.erase(std::remove_if(
+			coin_prop_list.begin(), coin_prop_list.end(),
+			[](CoinProp* coin_prop)
+			{
+				bool deleteable = coin_prop->can_remove();
+				if (deleteable) delete coin_prop;
+				return deleteable;
+			}), coin_prop_list.end());
+	}
+
+	void on_render(SDL_Renderer* renderer)
+	{
+		for (CoinProp* coin_prop : coin_prop_list)
+			coin_prop->on_render(renderer);
+	}
+
+	double get_current_coin_num()
+	{
+		return num_coin;
+	}
+
+	CoinPropList& get_coin_prop()
+	{
+		return coin_prop_list;
+	}
+
+	void spawn_coin_prop(const Vector2& position)
+	{
+		CoinProp* coin_prop = new CoinProp();
+		coin_prop->set_position(position);
+
+		coin_prop_list.push_back(coin_prop);
+	}
+
 protected:
 	CoinManager()
 	{
-
+		num_coin = ConfigManager::instance()->num_initial_coin;
 	}
 	~CoinManager()
 	{
-
+		for (CoinProp* coin_prop : coin_prop_list)
+			delete coin_prop;
 	}
 
 private:
 	double num_coin = 0;
+
+	CoinPropList coin_prop_list;
 
 };
 
