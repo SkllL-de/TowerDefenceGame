a2150cbed37f6783c6edd0c85766792812c54937
SkllL-de <skl4623@outlook.com>
2026-05-19 14:39:51 +0800

    新增：子弹与敌人碰撞逻辑，爆金币

--- TowerDefenceGame/enemy_manager.h
+++ TowerDefenceGame/enemy_manager.h
@@ -10,6 +10,8 @@
 #include "skeleton_enemy.h"
 #include "goblin_enemy.h"
 #include "goblin_priest_enemy.h"
+#include "bullet_manager.h"
+#include "coin_manager.h"
 
 #include <vector>
 
@@ -153,7 +155,53 @@ private:
 
 	void process_bullet_collision()
 	{
+		static BulletManager::BulletList& bullet_list =
+			BulletManager::instance()->get_bullet_list();
 
+		for (Enemy* enemy : enemy_list)
+		{
+			if (enemy->can_remove()) continue;
+
+			const Vector2& size_enemy = enemy->get_size();
+			const Vector2& pos_enemy = enemy->get_position();
+
+			for (Bullet* bullet : bullet_list)
+			{
+				if (!bullet->can_collide()) continue;
+
+				const Vector2 pos_bullet = bullet->get_position();
+
+				if(pos_bullet.x >= pos_enemy.x - size_enemy.x / 2
+					&& pos_bullet.y >= pos_enemy.y - size_enemy.y / 2
+					&& pos_bullet.x <= pos_enemy.x + size_enemy.x / 2
+					&& pos_bullet.y <= pos_enemy.y + size_enemy.y / 2)
+				{
+					double damage = bullet->get_damage();
+					double damage_range = bullet->get_damage_range();
+					if (damage_range < 0)
+					{
+						enemy->decrease_hp(damage);
+						if (enemy->can_remove())
+							try_swawn_coin_prop(pos_enemy, enemy->get_reward_ratio());
+					}
+					else
+					{
+						for (Enemy* target_enemy : enemy_list)
+						{
+							const Vector2 & pos_target_enemy = target_enemy->get_position();
+							if ((pos_target_enemy - pos_enemy).length() <= damage_range)
+							{
+								target_enemy->decrease_hp(damage);
+								if (target_enemy->can_remove())
+									try_swawn_coin_prop(pos_target_enemy, target_enemy->get_reward_ratio());
+							}
+						}
+					}
+
+					bullet->on_collide(enemy);//播放打击音效，部分子弹有减速等特殊效果。子弹设为invalid->true,collisional->false
+				}
+			}
+		}
 	}
 
 	void remove_invalid_enemy()
@@ -165,6 +213,13 @@ private:
 				if (deletable) delete enemy;
 				return deletable;
 			}), enemy_list.end());
+	}
+
+	void try_swawn_coin_prop(const Vector2& position, double ratio)
+	{
+		static CoinManager* instance = CoinManager::instance();
+		if ((double)(rand() % 100) / 100 <= ratio)
+			instance->spawn_coin_prop(position);
 	}
 };
 
