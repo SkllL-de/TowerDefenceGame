90ae4a4dfa2194662dc711102e27cf89287879d5
SkllL-de <skl4623@outlook.com>
2026-05-24 23:52:17 +0800

    新增：播放淡出音效功能。背景音乐改为淡入播放，BGM对应的track改为全局唯一。

--- TowerDefenceGame/audio_manager.h
+++ TowerDefenceGame/audio_manager.h
@@ -14,15 +14,23 @@ public:
 		MIX_PlayAudio(mixer, audio);
 	}
 
-	void PlayBGM(MIX_Audio* audio)
+	void PlayFadeOutMusic(MIX_Audio* audio,int ms)
 	{
 		MIX_Track* track = MIX_CreateTrack(mixer);
 		MIX_SetTrackAudio(track, audio);
+		MIX_PlayTrack(track, NULL);
+		MIX_StopTrack(track, MIX_TrackMSToFrames(track, ms));
+	}
+
+	void PlayBGM(MIX_Audio* audio, int fade_ms)
+	{
+		MIX_SetTrackAudio(bgm_track, audio);
 
 		SDL_PropertiesID props = SDL_CreateProperties();
+		SDL_SetNumberProperty(props, MIX_PROP_PLAY_FADE_IN_MILLISECONDS_NUMBER, fade_ms);
 		SDL_SetNumberProperty(props, MIX_PROP_PLAY_LOOPS_NUMBER, -1);//循环播放
 
-		MIX_PlayTrack(track, props);
+		MIX_PlayTrack(bgm_track, props);
 
 		SDL_DestroyProperties(props);
 	}
@@ -31,14 +39,17 @@ protected:
 	AudioManager()
 	{
 		mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+		bgm_track = MIX_CreateTrack(mixer);
 	}
 	~AudioManager()
 	{
+		MIX_DestroyTrack(bgm_track);
 		MIX_DestroyMixer(mixer);
 	}
 
 private:
 	MIX_Mixer* mixer = nullptr;
+	MIX_Track* bgm_track = nullptr;
 };
 
 #endif // !_AUDIO_MANAGER_H_
